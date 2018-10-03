1bef902e3e5cb792fa405a64ac9a6a3b1c7b5dcd
Aron Homberg <info@aron-homberg.de>
2018-10-03 18:53:49 +0200

    WebComponents WIP

--- scratch/di/SimpleStarter.ts
+++ scratch/di/SimpleStarter.ts
@@ -4,4 +4,5 @@ import {ApplicationContext} from "../../src/di/index";
 let simple = ApplicationContext.getInstance().getBean(Simple);
 let result = simple.calc(7, 7);
 
+
 console.log('result', result); // 42
