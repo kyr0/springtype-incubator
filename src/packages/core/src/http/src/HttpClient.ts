be2b1979f55257b1c3a7ad7bfeb8928c7bdc5cba
mansi1 <m.mansi1@gmx.net>
2019-05-02 17:52:13 +0200

    update client request

--- src/packages/core/src/http/src/HttpClient.ts
+++ src/packages/core/src/http/src/HttpClient.ts
@@ -1,13 +1,17 @@
 
+export  interface RequestInitial extends RequestInit{
+    mapper: (response: Response) => any;
+}
+
+
 export const JSON = (r: Response): any => {
     return r.json();
 };
 
 
-export const PLAIN_TEXT =  (r: Response): any => {
+export const PLAIN_TEXT = (r: Response): any => {
     return r.text();
 };
-
 
 export class AbortablePromise<T> implements Promise<T> {
 
@@ -17,13 +21,12 @@ export class AbortablePromise<T> implements Promise<T> {
 
     }
 
-
     catch<TResult = never>(onrejected?: ((reason: any) => (PromiseLike<TResult> | TResult)) | undefined | null): AbortablePromise<T | TResult> {
-        return new AbortablePromise(this.abortSignal,this.promise.catch(onrejected));
+        return new AbortablePromise(this.abortSignal, this.promise.catch(onrejected));
     }
 
     then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): AbortablePromise<TResult1 | TResult2> {
-      return  new AbortablePromise(this.abortSignal,this.promise.then(onfulfilled, onrejected));
+        return new AbortablePromise(this.abortSignal, this.promise.then(onfulfilled, onrejected));
     }
 
     abort() {
@@ -31,9 +34,11 @@ export class AbortablePromise<T> implements Promise<T> {
     }
 }
 
-export const send = (request: RequestInfo, mapper: (response: Response) => any = JSON): AbortablePromise<any> => {
+export const send = <T>(request: RequestInfo, init: RequestInitial = {mapper: JSON}): AbortablePromise<T> => {
     const abortController = new AbortController();
-    const response = fetch(request, {signal: abortController.signal}).then(mapper);
-    return new AbortablePromise(abortController, response);
+    const promise = fetch(request, {... init, signal: abortController.signal})
+        .then(init.mapper)
+        .then(v => v as T);
+    return new AbortablePromise(abortController, promise);
 };
 
